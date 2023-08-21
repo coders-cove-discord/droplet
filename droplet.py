9eb4bab9e5ba5a2964bf3f9b4f651f1d03c4a1ca
bkgrnd <124001257+StefanTheFork@users.noreply.github.com>
2023-08-21 20:53:32 +0300

    Add files via upload

---
+++ droplet.py
@@ -0,0 +1,16 @@
+# droplet. the app api thing for reverse
+
+name = "droplet"
+version = "v1"
+
+def init():
+    print("droplet has loaded!")
+    print(name)
+    print(version)
+    
+def example():
+    input(" admin\droplet.py >")
+    
+def apphandler():
+    pass
+    
